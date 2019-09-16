ed19282dc761698de81d3ced48b612f6f97f85e3
assane savadogo <assane.savadogo81@gmail.com>
2019-09-16 22:07:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,6 @@ require 'pry'
 def prime?(number)
   return false if number < 2 
   return true if number == 2
-    
-  (2..number-1).to_a.all? {|int|  number % int != 0}
+  (2..number-1).to_a.all? {|int| number % int != 0}
     #binding.pry
 end
